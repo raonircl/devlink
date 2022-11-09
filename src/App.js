4335af4b0cf4955c0d8cbb47b7c0d037118d1e32
raonircl <raoni.afi@gmail.com>
2022-11-09 03:21:35 -0300

    default function and first component(aluno)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  return(
+    <div>
+      <h1>Meu Primeiro Dia</h1>
+      <br/>    
+      <Aluno/>
     </div>
-  );
+
+  )    
 }
 
-export default App;
+function Aluno() {
+  return(
+    <>
+      <h2>Bem vindo ao seu primeiro dia de aula!</h2>
+      <p>Veja aqui os seus materias</p>
+      <button>
+        <strong>Clique aqui!</strong>
+      </button>
+    </>
+  )  
+}
