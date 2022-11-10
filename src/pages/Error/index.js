03bc2996b56629f28183d3b338abaabe0d4ae425
raonircl <raoni.afi@gmail.com>
2022-11-10 03:45:12 -0300

    css page error, create component logo

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,17 @@
+import './error.css'
+import { Link } from 'react-router-dom'
+import { Logo } from '../../components/Logo'
 
 export default function Error() {
   return(
-    <div>
+    <div className='error'>
+      <Logo/>
       <h1>Página não encontrada!</h1>
       <p>Essa página não existe.</p>
+
+      <Link className='link' to='/'>
+        Voltar para a página inicial!
+      </Link>
     </div>
   )  
 }
