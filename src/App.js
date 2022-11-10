0136c4f0ef0528c4297f46546691b0c7604584c7
raonircl <raoni.afi@gmail.com>
2022-11-10 00:42:33 -0300

    correction imports in app.js and index.js 'router'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 
 import { createBrowserRouter } from "react-router-dom"
-import Admin from "./pages/Admin"
 
-import Home from './pages/Admin'
+import Admin from './pages/Admin'
 import Home from './pages/Home'
-import Home from './pages/Login'
+import Login from './pages/Login'
 
 const router = createBrowserRouter([
   {
