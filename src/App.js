ce73ec5abd5c4ded72147601254508b57517c822
raonircl <raoni.afi@gmail.com>
2022-11-09 23:37:32 -0300

    reset css

--- src/App.js
+++ src/App.js
@@ -3,29 +3,7 @@ export default function App() {
   return(
     <div>
       <h1>Meu Primeiro Dia</h1>
-      <br/>    
-      <Aluno name="Raoni Cerqueira" dev="FullStack"/>
-      <Aluno name="Tatiana Lima" dev="Frontend"/>
     </div>
 
   )    
-}
-
-function Aluno({ name, dev }) {
-  return(
-    <>
-      <h2>Bem vindo {name} ao seu primeiro dia!</h2>
-      <h3 
-      style={{
-        color: "green"
-      }}
-      >
-        {dev}
-      </h3>
-      <p>Veja aqui os seus materias</p>
-      <button>
-        <strong>Clique aqui!</strong>
-      </button>
-    </>
-  )  
 }
