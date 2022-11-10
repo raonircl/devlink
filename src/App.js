07ccf69dfe4ff71ba357192dc61b1cd9259af0b5
raonircl <raoni.afi@gmail.com>
2022-11-10 00:32:49 -0300

    create app router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 
-export default function App() {
-  return(
-    <div>
-      <h1>Meu Primeiro Dia</h1>
-    </div>
+import { createBrowserRouter } from "react-router-dom"
+import Admin from "./pages/Admin"
 
-  )    
-}
+import Home from './pages/Admin'
+import Home from './pages/Home'
+import Home from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Admin/>
+  },
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/',
+    element: <Login/>
+  },
+
+])
+
+export { router };
